ea71bd2f8397c5c535b3ca157879e8dd3883fe84
keithpops <keith.poplawski@gmail.com>
2018-05-09 16:31:59 -0400

    homepage background placeholder

--- src/components/HomepageBackground.js
+++ src/components/HomepageBackground.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createPortal } from 'react-dom'
+import ReactPlayer from 'react-player'
 import v from 'vudu'
 import { styles as s } from 'stylesheet'
 
@@ -7,6 +8,9 @@ const localClasses = v({
   background: {
     '@composes': [s.bgBlack, s.fixed, s.l0, s.r0, s.t0, s.b0],
     zIndex: '1',
+    video: {
+      objectFit: 'cover',
+    },
   },
 })
 
@@ -14,7 +18,14 @@ const homepageBackgroundRoot = document.getElementById('background-root')
 
 const HomepageBackground = () => createPortal(
   <div className={localClasses.background}>
-
+    <ReactPlayer
+      width="100%"
+      height="100%"
+      playing
+      loop
+      volume={0}
+      url='https://ia801506.us.archive.org/33/items/loops_201805/safe_sex_slut-loop.mp4'
+    />
   </div>,
   homepageBackgroundRoot,
 )
